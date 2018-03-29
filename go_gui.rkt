01c027f81d650158c118d41231c106a59f82ee53
pranavrao870 <pranavrao870@gmail.com>
2018-03-29 20:02:20 +0530

    new files

---
+++ go_gui.rkt
@@ -0,0 +1,9 @@
+#lang racket
+
+(require "go_board.rkt")
+(require "go_engine.rkt")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;This will be the main file for the go game
+;this will instatiate the go board and the
+;go engine
